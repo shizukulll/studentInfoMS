f35d74873dedf9d8ddd8563c1a2efa9275ebeb88
shizukulll <126434177+shizukulll@users.noreply.github.com>
2024-04-01 15:51:02 +0800

    Update ConsoleObserver.java
    添加log模块

--- StudentInfoMS/src/Observer/ConsoleObserver.java
+++ StudentInfoMS/src/Observer/ConsoleObserver.java
@@ -1,17 +1,57 @@
 package Observer;
+
 import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.IOException;
 
 public class ConsoleObserver implements Observer {
     private final PrintWriter writer;
+    private static final String LOGS_DIRECTORY = "logs";
+    private static final DateTimeFormatter DATE_FOLDER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter TIMESTAMP = DateTimeFormatter.ofPattern("HH-mm-ss");
+
+    private Path logFilePath;
+
+    private boolean isFileNameInitialized = false;
 
     public ConsoleObserver() {
-        this.writer = new PrintWriter(System.out, true);//ljy
+        this.writer = new PrintWriter(System.out, true);
     }
 
     @Override
     public void update(String message) {
         writer.println(message);
         writer.flush();
+
+        if (!isFileNameInitialized) {
+            LocalDateTime now = LocalDateTime.now();
+            String dateFolder = DATE_FOLDER.format(now.toLocalDate());
+            String timestamp = TIMESTAMP.format(now);
+            String logFileName = timestamp + ".log";
+            Path logFileDir = Paths.get(LOGS_DIRECTORY, dateFolder);
+
+            try {
+                Files.createDirectories(logFileDir);
+                logFilePath = logFileDir.resolve(logFileName);
+                Files.createFile(logFilePath);
+                isFileNameInitialized = true;
+            } catch (IOException e) {
+                System.err.println("Error creating log file: " + e.getMessage());
+            }
+        }
+
+        String msg = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " - " + message + System.lineSeparator();
+
+        try {
+            Files.write(logFilePath, msg.getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.err.println("Error writing to log file: " + e.getMessage());
+        }
     }
 }
